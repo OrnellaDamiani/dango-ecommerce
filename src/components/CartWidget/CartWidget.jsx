802f31b38bf3208efde3baed0016a42dcd614258
Ornella <orne.damiani@gmail.com>
2023-07-07 13:21:10 -0300

    Agrego CartWidget

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+const CartWidget = () => {
+    return (
+        <>
+            <img src="src/img/cart.png" />
+            <div>
+                <p>3</p>
+            </div>
+        </>
+
+    )
+}
+
+export default CartWidget;
