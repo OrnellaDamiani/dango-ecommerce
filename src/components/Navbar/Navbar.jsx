5acebafd1e2b729876864ba046c4357ff8cb06c5
Ornella <orne.damiani@gmail.com>
2023-07-11 18:39:05 -0300

    DAMIANI-DANGO

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,12 @@ import CartWidget from '../CartWidget/CartWidget';
 
 const Navbar = () => {
     return (
-        <nav className='navbar'>
+        <body>
+            <header class="header">
             <div className='logo'>
                 <img src='src/img/logo.png' />
             </div>
+        <nav className='navbar'>
             <div className='menu'>
                 <Button texto="inicio" className="navbar-button" accion={() => console.log("Clickeaste Inicio")} />
                 <Button texto="anime" className="navbar-button" accion={() => console.log("Clickeaste Anime")} />
@@ -18,7 +20,35 @@ const Navbar = () => {
                 <CartWidget />
             </div>
         </nav>
+        </header>
+        </body>
+        
     )
 }  
 
 export default Navbar;
+{/* <body>
+    <header class="header">
+        <div class="logo">
+            <img src="../img/logo.png" alt="logo" />
+        </div>
+        <nav class="navbar">
+            <ul class="menu">
+                <Button texto="inicio" className="navbar-button">Inicio</Button>
+                <Button texto="anime" className="navbar-button">Anime</Button>
+                <Button texto="manga" className="navbar-button">Manga</Button>
+                <Button texto="manga" className="navbar-button">Contacto</Button>
+
+            </ul>
+
+        </nav>
+        
+        <div class="cart-widget">
+            <img src="../img/cart2.png" class="img"/>
+            <div>
+                <p>3</p>
+            </div>
+        </div>
+
+    </header>
+</body> */}
