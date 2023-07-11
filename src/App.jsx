a424a8b5b88c068d73afe90f35e1c98a45f391e3
Ornella <orne.damiani@gmail.com>
2023-07-10 21:08:55 -0300

    DAMIANI-DANGO

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-//import './App.css'//
+import './App.css'
 import Navbar from'./components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 function App() {
