fb8d4a6b824e6201020bbb7f0fc03810ef4b20ab
Ornella <orne.damiani@gmail.com>
2023-07-26 20:32:41 -0300

    DAMIANI-DANGO

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,33 @@
-const ItemListContainer = ({ greeting }) => {
-    return (
-        <h2 className="item-list-container">
-            {greeting}
-        </h2>
-    )
-}
+import React, { useEffect, useState } from 'react';
+import ItemList from '../ItemList/ItemList';
+import { getProducts } from '../../mock/data';
+import { useParams } from 'react-router-dom';
 
-export default ItemListContainer;     
+
+const ItemListContainer = ({ greeting }) => {
+  const [productsList, setProductsList] = useState([]);
+  const {categoryId}= useParams()
+
+  useEffect(() => {
+    getProducts()
+      .then((res) => {
+        if(categoryId){
+          setProductsList(res.filter((item)=> item.category === categoryId))
+
+        } else {
+          setProductsList(res)
+        }
+      } )
+
+      .catch((error) => console.log(error));
+  }, [categoryId]);
+
+  return (
+    <div>
+      <h2 className='item-list-container'>{greeting}</h2>
+      <ItemList productsList={productsList} />
+    </div>
+  );
+};
+
+export default ItemListContainer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Button from '../Button/Button'
 import CartWidget from '../CartWidget/CartWidget';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <body>
             <header class="header">
-            <div className='logo'>
+            <div className='logo' as={NavLink} to='/'>
                 <img src='src/img/logo.png' />
             </div>
         <nav className='navbar'>
             <div className='menu'>
-                <Button texto="inicio" className="navbar-button" accion={() => console.log("Clickeaste Inicio")} />
-                <Button texto="anime" className="navbar-button" accion={() => console.log("Clickeaste Anime")} />
-                <Button texto="manga" className="navbar-button" accion={() => console.log("Clickeaste Manga")} />
+                <Button texto="inicio" className="navbar-button"  as={NavLink} to='/' />
+                <Button texto="anime" className="navbar-button" as={NavLink} to='/category/Anime' />
+                <Button texto="manga" className="navbar-button" as={NavLink} to='category/Manga' />
                 <Button texto="contacto" className="navbar-button" accion={() => console.log("Clickeaste Contacto")} />
             </div>
             <div className='cart'>
